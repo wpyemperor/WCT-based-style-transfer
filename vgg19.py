616a60ab247d688e5d1138db690b4475fa12609b
wpyemperor <wpyemperor@163.com>
2019-12-08 14:36:29 -0500

    Update vgg19.py

--- vgg19.py
+++ vgg19.py
@@ -4,7 +4,75 @@ import numpy as np
 import inspect
 
 VGG_MEAN = [103.939, 116.779, 123.68]
+def get_wav(in_channels, pool=True):
+    """wavelet decomposition using conv2d"""
+    harr_wav_L = 1 / np.sqrt(2) * np.ones((1, 2))
+    harr_wav_H = 1 / np.sqrt(2) * np.ones((1, 2))
+    harr_wav_H[0, 0] = -1 * harr_wav_H[0, 0]
 
+    harr_wav_LL = np.transpose(harr_wav_L) * harr_wav_L
+    harr_wav_LH = np.transpose(harr_wav_L) * harr_wav_H
+    harr_wav_HL = np.transpose(harr_wav_H) * harr_wav_L
+    harr_wav_HH = np.transpose(harr_wav_H) * harr_wav_H
+
+    filter_LL = torch.from_numpy(harr_wav_LL).unsqueeze(0)
+    filter_LH = torch.from_numpy(harr_wav_LH).unsqueeze(0)
+    filter_HL = torch.from_numpy(harr_wav_HL).unsqueeze(0)
+    filter_HH = torch.from_numpy(harr_wav_HH).unsqueeze(0)
+
+    if pool:
+        net = nn.Conv2d
+    else:
+        net = nn.ConvTranspose2d
+
+    LL = net(in_channels, in_channels,
+             kernel_size=2, stride=2, padding=0, bias=False,
+             groups=in_channels)
+    LH = net(in_channels, in_channels,
+             kernel_size=2, stride=2, padding=0, bias=False,
+             groups=in_channels)
+    HL = net(in_channels, in_channels,
+             kernel_size=2, stride=2, padding=0, bias=False,
+             groups=in_channels)
+    HH = net(in_channels, in_channels,
+             kernel_size=2, stride=2, padding=0, bias=False,
+             groups=in_channels)
+
+    LL.weight.requires_grad = False
+    LH.weight.requires_grad = False
+    HL.weight.requires_grad = False
+    HH.weight.requires_grad = False
+
+    LL.weight.data = filter_LL.float().unsqueeze(0).expand(in_channels, -1, -1, -1)
+    LH.weight.data = filter_LH.float().unsqueeze(0).expand(in_channels, -1, -1, -1)
+    HL.weight.data = filter_HL.float().unsqueeze(0).expand(in_channels, -1, -1, -1)
+    HH.weight.data = filter_HH.float().unsqueeze(0).expand(in_channels, -1, -1, -1)
+
+    return LL, LH, HL, HH
+
+class WavePool(nn.Module):
+    def __init__(self, in_channels):
+        super(WavePool, self).__init__()
+        self.LL, self.LH, self.HL, self.HH = get_wav(in_channels)
+
+    def forward(self, x):
+        return self.LL(x), self.LH(x), self.HL(x), self.HH(x)
+
+
+class WaveUnpool(nn.Module):
+    def __init__(self, in_channels, option_unpool='cat5'):
+        super(WaveUnpool, self).__init__()
+        self.in_channels = in_channels
+        self.option_unpool = option_unpool
+        self.LL, self.LH, self.HL, self.HH = get_wav(self.in_channels, pool=False)
+
+    def forward(self, LL, LH, HL, HH, original=None):
+        if self.option_unpool == 'sum':
+            return self.LL(LL) + self.LH(LH) + self.HL(HL) + self.HH(HH)
+        elif self.option_unpool == 'cat5' and original is not None:
+            return torch.cat([self.LL(LL), self.LH(LH), self.HL(HL), self.HH(HH), original], dim=1)
+        else:
+            raise NotImplementedError
 
 class Vgg19:
     def __init__(self, vgg19_npy_path=None):
@@ -14,10 +82,10 @@ class Vgg19:
             path = os.path.join(path, "vgg19.npy")
             vgg19_npy_path = path
             print(vgg19_npy_path)
-
+		self.pool=WavePool()
         self.data_dict = np.load(vgg19_npy_path, encoding='latin1').item()
         print("npy file loaded")
-
+		
     def encoder(self,inputs,target_layer):
         layer_num =dict(zip(['relu1','relu2','relu3','relu4','relu5'],range(1,6)))[target_layer]
         encode = inputs
@@ -42,19 +110,18 @@ class Vgg19:
                      ('conv5_2',512),
                      ('conv5_3',512),
                      ('conv5_4',512),]}  
-                
+               
         for d in range(1,layer_num+1):
             for layer in encoder_arg[str(d)]:                
                 if 'conv' in layer[0] :
                     encode =self.conv_layer(encode,layer[0])
                 if 'pool' in layer[0] and d <layer_num :
-                    encode = self.max_pool(encode,layer[0])               
+                    encode = self.pool(encode,layer[0])               
         return encode
     
     def encoder_all_layer(self, inputs):
         """
         load variable from npy to build the VGG
-
         :param rgb: rgb image [batch, height, width, 3] values scaled [0, 1]
         """
 
@@ -62,17 +129,17 @@ class Vgg19:
         encode = inputs
         conv1_1 = self.conv_layer(encode, "conv1_1")
         conv1_2 = self.conv_layer(conv1_1, "conv1_2")
-        pool1 = self.max_pool(conv1_2, 'pool1')
+        pool1 = self.pool(conv1_2, 'pool1')
 
         conv2_1 = self.conv_layer(pool1, "conv2_1")
         conv2_2 = self.conv_layer(conv2_1, "conv2_2")
-        pool2 = self.max_pool(conv2_2, 'pool2')
+        pool2 = self.pool(conv2_2, 'pool2')
 
         conv3_1 = self.conv_layer(pool2, "conv3_1")
         conv3_2 = self.conv_layer(conv3_1, "conv3_2")
         conv3_3 = self.conv_layer(conv3_2, "conv3_3")
         conv3_4 = self.conv_layer(conv3_3, "conv3_4")
-        pool3 = self.max_pool(conv3_4, 'pool3')
+        pool3 = self.pool(conv3_4, 'pool3')
 
         conv4_1 = self.conv_layer(pool3, "conv4_1")
         conv4_2 = self.conv_layer(conv4_1, "conv4_2")
@@ -90,7 +157,7 @@ class Vgg19:
 
     def max_pool(self, bottom, name):
         return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
-
+		
     def conv_layer(self, bottom, name):
         with tf.variable_scope(name):
             filt = self.get_conv_filter(name)
